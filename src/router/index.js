9121ca70decf01e50d474d8f378a0f3337e1d4fd
vic lotana <viclotana@gmail.com>
2018-07-08 23:47:55 +0100

    added vuematerial

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 Vue.use(Router)
 
