16773a4c1da35304874f7a37813e6969d17ebb4c
vic lotana <viclotana@gmail.com>
2018-07-09 01:35:30 +0100

    adding cards

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,40 @@
 <template>
  <div class="menu">
-  <h1>{{title}}</h1>
+    <md-card class="md-card-example">
+      <md-card-area md-inset>
+        <md-card-media md-ratio="16:9">
+          <img src="/assets/examples/card-example.jpg" alt="Coffee House">
+        </md-card-media>
+
+        <md-card-header>
+          <h2 class="md-title">Coffee House</h2>
+          <div class="md-subhead">
+            <md-icon>location_on</md-icon>
+            <span>2 miles</span>
+          </div>
+        </md-card-header>
+
+        <md-card-content>
+          Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.
+        </md-card-content>
+      </md-card-area>
+
+      <md-card-content>
+        <h3 class="md-subheading">Today's availability</h3>
+        <div class="card-reservation">
+          <md-icon>access_time</md-icon>
+          <div class="md-button-group">
+            <md-button>5:30PM</md-button>
+            <md-button>7:30PM</md-button>
+            <md-button>9:00PM</md-button>
+          </div>
+        </div>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button class="md-primary">Reserve</md-button>
+      </md-card-actions>
+    </md-card>
  </div>
 </template>
 
